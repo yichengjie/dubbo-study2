678d62aa0bf14193838a5c06f4c58f6f267307de
易成杰 <626659321@qq.com>
2020-10-02 14:04:47 +0800

    基于注解的dubbo

--- hello-consumer/src/main/java/com/yicj/hello/controller/HelloController.java
+++ hello-consumer/src/main/java/com/yicj/hello/controller/HelloController.java
@@ -1,14 +1,14 @@
 package com.yicj.hello.controller;
 
 import com.yicj.hello.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Autowired
+    @DubboReference(version = "1.0.0")
     private UserService userService ;
 
     @GetMapping("/hello")
--- hello-provider/src/main/java/com/yicj/hello/HelloProviderApp.java
+++ hello-provider/src/main/java/com/yicj/hello/HelloProviderApp.java
@@ -1,13 +1,15 @@
 package com.yicj.hello;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-
 import java.io.IOException;
 
-@ImportResource("classpath:dubbo/provider.xml")
+
 @SpringBootApplication
+@ImportResource("classpath:dubbo/provider.xml")
+@EnableDubbo(scanBasePackages = "com.yicj.hello.service.impl")
 public class HelloProviderApp {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(HelloProviderApp.class, args) ;
