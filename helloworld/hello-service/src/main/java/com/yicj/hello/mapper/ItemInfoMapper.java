88df398f5bc274412f48d7e5ed85d01402fc08f3
易成杰 <626659321@qq.com>
2020-10-02 18:44:25 +0800

    保存

--- helloworld/hello-service/src/main/java/com/yicj/hello/mapper/ItemInfoMapper.java
+++ helloworld/hello-service/src/main/java/com/yicj/hello/mapper/ItemInfoMapper.java
@@ -3,6 +3,6 @@ package com.yicj.hello.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yicj.hello.model.entity.ItemInfo;
 
-public interface ItemInfoMapper extends BaseMapper<ItemInfo> {
 
+public interface ItemInfoMapper extends BaseMapper<ItemInfo> {
 }
--- helloworld/hello-service/src/test/java/com/yicj/hello/mapper/ItemInfoMapperTest.java
+++ helloworld/hello-service/src/test/java/com/yicj/hello/mapper/ItemInfoMapperTest.java
@@ -1,13 +1,15 @@
 package com.yicj.hello.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yicj.hello.model.entity.ItemInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Slf4j
@@ -15,7 +17,7 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 public class ItemInfoMapperTest {
 
-    @Autowired
+    @Resource
     private ItemInfoMapper itemInfoMapper ;
 
     @Test
@@ -23,5 +25,14 @@ public class ItemInfoMapperTest {
         log.info("-------------- selectAll method test ------------");
         List<ItemInfo> itemInfos = itemInfoMapper.selectList(null);
         itemInfos.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectPage(){
+        IPage<ItemInfo> page = new Page<>(1,2) ;
+        //参数一是当前页，参数二是每页个数
+        IPage<ItemInfo> itemInfoIPage = itemInfoMapper.selectPage(page, null);
+        List<ItemInfo> records = itemInfoIPage.getRecords();
+        records.forEach(item -> log.info("====> item : {}", item));
     }
 }
