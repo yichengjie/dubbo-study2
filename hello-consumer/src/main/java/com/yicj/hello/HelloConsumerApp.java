d44cb1022c5201ba8382795857e9d8ec5bb7ab09
易成杰 <626659321@qq.com>
2020-10-02 11:24:40 +0800

    dubbo hello world

--- hello-consumer/src/main/java/com/yicj/hello/HelloConsumerApp.java
+++ hello-consumer/src/main/java/com/yicj/hello/HelloConsumerApp.java
@@ -1,11 +1,9 @@
 package com.yicj.hello;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableDubbo
 @SpringBootApplication
 @ImportResource("classpath:dubbo/consumer.xml")
 public class HelloConsumerApp {
